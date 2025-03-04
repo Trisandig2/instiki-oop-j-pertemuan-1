6d97ca1795f6ff23b7e44a7977f10c4936956097
PC-F-21 <email akun Githubgootrisandi@gmail.com>
2025-03-04 21:44:47 +0800

    upload pertama

---
+++ Pertemuan01/src/pertemuan01/Pertemuan01.java
@@ -0,0 +1,17 @@
+
+package pertemuan01;
+
+/**
+ *
+ * @author Trisandi arista go'o wea
+ */
+public class Pertemuan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Belajar Bahasa ava");
+    }
+    
+}
